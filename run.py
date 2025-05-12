2cfccc00cae399c355ae93d01a813907d7b27b4f
Ernesto Aizenberg <sereernest@gmail.com>
2025-05-12 06:30:43 +0300

    update

--- run.py
+++ run.py
@@ -1,20 +1,25 @@
+import os
+import subprocess
+
 from app import create_app
 from config import Config, RedisConfig
-import subprocess
 
 if __name__ == "__main__":
     app = create_app(
         app_config=Config(),
         redis_config=RedisConfig(),
     )
-    subprocess.run(
-       [
-            "redis-server",
-            "--daemonsize"
-            "yes"
-        ],
-        check=True
-     )
+    redis_run = os.getenv("REDIS_RUN", "true").lower() == "true"
+    if redis_run:
+        print("Running Redis...")
+        subprocess.run(
+            [
+                "redis-server",
+                "--daemonize",
+                "yes"
+             ],
+             check=True
+         )
     app.run(
         debug=app.config.get("DEBUG"),
         port=app.config.get("PORT"),
